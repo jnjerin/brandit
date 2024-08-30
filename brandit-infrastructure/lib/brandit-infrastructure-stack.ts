1ede4b05f81c26cdbe2ddfc72da65961db7cd591
jnjerin <njerijoan24@gmail.com>
2024-08-30 16:50:27 +0300

    remove exception from gitignore for lambda stack file

---
+++ brandit-infrastructure/lib/brandit-infrastructure-stack.ts
@@ -0,0 +1,30 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+export class BranditInfrastructureStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const layer = new lambda.LayerVersion(this, "BaseLayer", {
+      code: lambda.Code.fromAsset("lambda_base_layer/layer.zip"),
+      compatibleRuntimes: [lambda.Runtime.PYTHON_3_9],
+    });
+
+    // Create a Lambda function
+    const apiLambda = new lambda.Function(this, "ApiFunction", {
+      runtime: lambda.Runtime.PYTHON_3_9,
+      code: lambda.Code.fromAsset("../app/"),
+      handler: "brandkit_api.handler",
+      layers: [layer],
+      environment: {
+        OPENAI_API_KEY: process.env.OPENAI_API_KEY ?? "",
+      },
+      });
+    
+  }
+}
