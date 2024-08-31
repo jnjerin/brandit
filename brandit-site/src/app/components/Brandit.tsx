544d2009ac49233ab3f97e3f43592957308f1ba6
jnjerin <njerijoan24@gmail.com>
2024-08-31 16:54:04 +0300

    create Form component to input prompt

--- brandit-site/src/app/components/Brandit.tsx
+++ brandit-site/src/app/components/Brandit.tsx
@@ -1,12 +1,16 @@
 'use client'
 
 import React from 'react'
+import Form from './Form';
 
 const Brandit = () => {
   const ENDPOINT: string =
     "https://9sxyr49ks0.execute-api.us-east-1.amazonaws.com/prod/generate_snippet_and_keywords";
 
-  const [prompt, setPrompt] = React.useState('')
+  const [prompt, setPrompt] = React.useState("");
+  const [snippet, setSnippet] = React.useState("");
+  const [keywords, setKeywords] = React.useState([]);
+  const [hasResult, setHasResult] = React.useState(false);
 
   const onSubmit = () => {
     console.log('submitting' + prompt)
@@ -14,19 +18,10 @@ const Brandit = () => {
   }
 
   return (
-    <>
-      <h1>Brandit</h1>
-      <p>
-          Tell me what your brand is about and i will generate copy and keywords for you
-      </p>
-      <input 
-        type='text' 
-        placeholder='cake'
-        value={prompt}
-        onChange={(e) => setPrompt(e.target.value)}
-      ></input>
-      <button onClick={onSubmit}>Generate</button>
-    </>
+    <div>
+      <Form />
+    </div>
+   
   )
 }
 
---
+++ brandit-site/src/app/components/Form.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+interface FormProps {
+    prompt: string;
+    setPrompt: any;
+    onSubmit: any;
+  }
+
+const Form: React.FC<FormProps> = (props) => {
+
+  return (
+    <>
+        <div className="">
+            <p>
+                Tell me what your brand is about and I will generate copy and keywords
+                for you.
+            </p>
+        </div>
+
+        <input
+        className=""
+        type="text"
+        placeholder="cake"
+        value={props.prompt}
+        onChange={(e) => prompt(e.currentTarget.value)}
+        ></input>
+    <button
+      className=""
+      onClick={props.onSubmit}
+    >
+      Submit
+    </button>
+  </>
+  )
+}
+
+export default Form
