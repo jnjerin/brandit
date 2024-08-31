8b5e605090b222b2890286ef1b205e16e1b50da5
jnjerin <njerijoan24@gmail.com>
2024-08-31 15:21:49 +0300

    link API endpoint to submit prompt data

--- brandit-site/src/app/components/Brandit.tsx
+++ brandit-site/src/app/components/Brandit.tsx
@@ -3,7 +3,15 @@
 import React from 'react'
 
 const Brandit = () => {
+  const ENDPOINT: string =
+    "https://9sxyr49ks0.execute-api.us-east-1.amazonaws.com/prod/generate_snippet_and_keywords";
+
   const [prompt, setPrompt] = React.useState('')
+
+  const onSubmit = () => {
+    console.log('submitting' + prompt)
+    fetch(`${ENDPOINT}?prompt=${prompt}`).then(console.log)
+  }
 
   return (
     <>
@@ -11,8 +19,13 @@ const Brandit = () => {
       <p>
           Tell me what your brand is about and i will generate copy and keywords for you
       </p>
-      <input type='text' placeholder='cake'></input>
-      <button>Generate</button>
+      <input 
+        type='text' 
+        placeholder='cake'
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
+      ></input>
+      <button onClick={onSubmit}>Generate</button>
     </>
   )
 }
